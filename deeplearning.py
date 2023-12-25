79d073556c6ed46142932b318f42fd5e78a2f5a8
mojtaba nafez <mojtabanafez96@gmail.com>
2023-12-26 01:37:49 +0330

    update

--- deeplearning.py
+++ deeplearning.py
@@ -94,7 +94,7 @@ def eval(data_loader, model, device, batch_size=64, print_perform=False):
         batch_y_predict = model(batch_x)
         loss = criterion(batch_y_predict, batch_y)
         batch_y_predict = torch.argmax(batch_y_predict.detach().cpu(), dim=1)
-        y_true.append(batch_y)
+        y_true.append(batch_y.cpu())
         y_predict.append(batch_y_predict)
         loss_sum.append(loss.item())
     
