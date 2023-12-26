3798c1511132cea240dd824c5d8f6558d5c86a0b
mojtaba nafez <mojtabanafez96@gmail.com>
2023-12-26 12:24:26 +0330

    update

--- deeplearning.py
+++ deeplearning.py
@@ -65,7 +65,10 @@ def ood_eval(data_loader, model, device, batch_size=64, print_perform=False):
         batch_y_predict = model(batch_x)
         loss = criterion(batch_y_predict, batch_y)
         batch_y_predict = torch.max(batch_y_predict.detach().cpu(), dim=1).values
+        print("batch_y_predict: ", batch_y_predict)
         y_true.append(batch_y.cpu())
+        print("batch_y: ", batch_y)
+
         y_predict.append(batch_y_predict)
         loss_sum.append(loss.item())
     
